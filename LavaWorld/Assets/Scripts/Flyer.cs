2aaa52163636f9749c55568b4ee58c89c38bb2ba
rj-90 <rehaf@rehafs-mbp-2.american.edu>
2016-03-23 19:59:25 -0400

    testing
    testing

---
+++ LavaWorld/Assets/Scripts/Flyer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// just for testing
+///added a new comment 
+/// </summary>
+public class Flyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

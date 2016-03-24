97b13205b54e1365d01cf96f3f8f2fcc95d47ba2
Coffee-Goat <dpetricca@mac.com>
2016-03-24 17:25:44 -0400

    added a file
    comment

---
+++ LavaWorld/Assets/Scripts/Hi.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hi : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	//hi test from dan 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

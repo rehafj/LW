e5f724175cb34ddbb765bcf805a6506814dd69e3
rj-90 <rehaf@rehafs-MBP-2.fios-router.home>
2016-03-24 22:21:07 -0400

    testing scripts
    just a few tests for functionality  - delete em later

--- LavaWorld/Assets/Scripts/LavaEffects.cs
+++ LavaWorld/Assets/Scripts/LavaEffects.cs
@@ -89,5 +89,4 @@ public bool RetrunWasDestroyed (){
 		return wasDestroyed;
 }
 
-
 }
---
+++ LavaWorld/Assets/Testting.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Testting : MonoBehaviour {
+
+public Renderer myrnd ;
+	// Use this for initialization
+	void Start () {
+	myrnd = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// this would disbale the script ( this sctipr - not others) first option if it isnt 
+	/// </summary>
+	void OnBecameVisible() {
+	// gameObject.SetActive(true);
+	 Debug.Log("Object is visible");
+	 enabled = true;
+
+        }
+
+	void OnBecameInvisible() {
+	enabled = false;
+		//gameObject.SetActive(false);
+		Debug.Log("Object is invisble from cam");
+
+
+       // enabled = false;
+    }
+}

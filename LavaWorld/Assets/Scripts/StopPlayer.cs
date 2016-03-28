f6ee4bf1eb29820af8f7bb98377eea3f2cc1e13b
rj-90 <rehaf@rehafs-mbp-2.american.edu>
2016-03-28 19:25:17 -0400

    stopped player movement
    enable player movement - after whatever happens

--- LavaWorld/Assets/Scripts/StopPlayer.cs
+++ LavaWorld/Assets/Scripts/StopPlayer.cs
@@ -18,7 +18,7 @@ using System.Collections;
    // voidUpdate()
   //  {
      //   if (BlockInspectorEditor == true) {
-           Directions.idle;
+//           Directions.idle;
       //  }
 
   //  }

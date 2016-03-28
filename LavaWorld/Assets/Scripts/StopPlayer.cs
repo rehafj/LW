8a7ab5716070dc983292e5b4d685a7c07914cc59
BriSyWill <bw5095a@american.edu>
2016-03-28 17:52:42 -0400

    oh my god do I try
    I try all the time
    In this institution

---
+++ LavaWorld/Assets/Scripts/StopPlayer.cs
@@ -0,0 +1,25 @@
+﻿///using UnityEngine;
+using System.Collections;
+
+///public class StopPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	///void Start () {
+	
+	///}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+//}
+//public class PlayerControllerDisable {
+    //public bool BlockInspectorEditor { get; private set; }  
+   // voidUpdate()
+  //  {
+     //   if (BlockInspectorEditor == true) {
+           Directions.idle;
+      //  }
+
+  //  }
+//}

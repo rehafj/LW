cbbcb97d448a38034d9558c668868cbc5945da5f
rj-90 <rehaf@rehafs-MBP-2.fios-router.home>
2016-03-29 11:48:55 -0400

    added mini narrative controller script
    to be used interchangibly as a method from the narrative flow chart

---
+++ LavaWorld/Assets/NarrativeController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fungus;
+
+public class NarrativeController : MonoBehaviour {
+public PlayerController plr;
+	// Use this for initialization
+	void Start () {
+	plr = FindObjectOfType<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void   ActivatePlayerPlatform(){
+		plr.enabled= true;
+
+	}
+}

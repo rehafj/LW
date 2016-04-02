97265229b9190c7c86a7f875669ff407d6546d02
rj-90 <rehaf@rehafs-MBP-2.fios-router.home>
2016-04-02 01:53:24 -0400

    parallaxe testing
    check out level test

---
+++ LavaWorld/Assets/levels/rehaftilesetupdate5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="rehaftilesetupdate5" tilewidth="32" tileheight="32" tilecount="196" columns="14">
+ <image source="../../../../pixelstuff/rehaftilesetupdate5.png" width="448" height="448"/>
+</tileset>

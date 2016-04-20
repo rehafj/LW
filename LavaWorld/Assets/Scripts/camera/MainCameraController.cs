eeb67feddac2419ebf784af9a93bc470d4152078
Coffee-Goat <dpetricca@mac.com>
2016-04-19 21:00:01 -0400

    DAN CODE DO NOT TOUCH

--- LavaWorld/Assets/Scripts/camera/MainCameraController.cs
+++ LavaWorld/Assets/Scripts/camera/MainCameraController.cs
@@ -2,47 +2,53 @@
 using System.Collections;
 
 public class MainCameraController : MonoBehaviour {
+    //Derrives from MonoBehavior
 
 	// Use this for initialization
    //public Vector3 postionI; 
    //public Vector3 targetPos;
    public GameObject PlayerPosition;
    PlayerController playerdirection;
+    //playerdirect lets the script access stuff from the player script.  Just a name
    public Vector3 targetlocation;
+    // Vector 3 takes X,Y, and Z does not take rotation
    public float smoothing = 0;
    public float xVal, Yval;
+    float xPosition, yPosition, zPosition;
 
+    //Use this for initialization
    void Start()
-   {
+    {
        PlayerPosition = GameObject.FindGameObjectWithTag("Player");
        playerdirection = PlayerPosition.GetComponent<PlayerController>();
-       //PlayerPostion = PlayerPostion.GetComponent<GameObject>();
+       //PlayerPosition = PlayerPosition.GetComponent<GameObject>();
 
    }
-   /// <summary>
-   /// do you guys want this script - just adds space infront of the player - or we can manually go and change the x value and just have lerp here - notetoself
-   /// </summary>
-
-   // Update is called once per frame
    void FixedUpdate()
-   {//// if our camera is not a child we can implement ths not oto show the flow but we habe tp scale it right amd this will save the y pos of the camera 
-    ///right not its relative to the player 
-       targetlocation = new Vector3(PlayerPosition.transform.position.x, transform.position.y, transform.position.z);
+   {
+  /*      targetlocation = new Vector3(PlayerPosition.transform.position.x, PlayerPosition.transform.position.y, transform.position.z);
+        //So it can maintain its own Z value
 
-       if (playerdirection.isFacingRight)
+        if (playerdirection.isFacingRight)
        {
            //transform.position 
             targetlocation = new Vector3(PlayerPosition.transform.position.x + xVal, PlayerPosition.transform.position.y + Yval, transform.position.z);
+            //makes the player look like she is on the left side of the screen, moves the camera right
            transform.position = Vector3.Lerp(transform.position, targetlocation, smoothing * Time.deltaTime);
+            //lerp takes two things by a fraction of third value
        }
        else if (!playerdirection.isFacingRight)
        {
 			targetlocation = new Vector3(PlayerPosition.transform.position.x - xVal, PlayerPosition.transform.position.y+Yval, transform.position.z);
           transform.position = Vector3.Lerp(transform.position, targetlocation, smoothing * Time.deltaTime);
        }
-        var xPosition = PlayerPosition.transform.position.x;
-         var yPosition = transform.position.y;
-         var zPosition = transform.position.z;
+       //this one is based on the player's direction and would change the camera
+       */
+         xPosition = PlayerPosition.transform.position.x;
+         yPosition = PlayerPosition.transform.position.y;
+         zPosition = transform.position.z;
+        //var means that the value is undefined in that particular instance or that it can change
+        //I defined these on a local scope b/c I didnt think we'd use it anywhere
          //transform.position 
          if (PlayerPosition.transform.position.x > 100 || PlayerPosition.transform.position.x < 0)
          {
@@ -52,10 +58,12 @@ public class MainCameraController : MonoBehaviour {
          {
              yPosition = targetlocation.y;
          }
+         else { }
          targetlocation = new Vector3(xPosition, yPosition, zPosition);
 
 
-         transform.position = Vector3.Lerp(transform.position, targetlocation, 2f * Time.deltaTime); 
+         transform.position = Vector3.Lerp(transform.position, targetlocation, 10f * Time.deltaTime); 
+        //based on defined values and is updated
    }
 }
 
